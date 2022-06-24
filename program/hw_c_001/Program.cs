cee01bb38af7338bc893598a46bb348e135d6766
sergey <ootieroo@gmail.com>
2022-06-24 21:30:51 +0400

    первая задача

---
+++ program/hw_c_001/Program.cs
@@ -0,0 +1,16 @@
+﻿// home work c#
+Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if  (a > b) 
+{
+Console.WriteLine("{0} наибольшее число", a);
+}
+else
+{
+Console.WriteLine("{0} наибольшее число", b);
+}
+    Console.ReadLine();
